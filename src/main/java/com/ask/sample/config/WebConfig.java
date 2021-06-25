bb9be75906c9ac039c9cfaf82d5c7f1740356fd6
csh0034 <csh0034@gmail.com>
2021-06-25 16:31:54 +0900

    feat StaticResourceLocation 은 security에서 cors 처리를 하지 않으므로 WebConfig 에 추가

--- src/main/java/com/ask/sample/config/WebConfig.java
+++ src/main/java/com/ask/sample/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.ask.sample.config;
 import com.ask.sample.util.JwtUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public JwtUtils jwtUtils(SettingProperties settingProperties) {
         return new JwtUtils(settingProperties.getJwtSecret());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(1800L);
     }
 }
